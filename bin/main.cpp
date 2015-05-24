1c0fed1acfe2b97ce06c2af9d4cc770dfd75c4ce
Andras Kucsma <andras.kucsma@prezi.com>
2015-05-24 13:08:31 +0200

    Add SFML

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 #include "sample.hpp"
 
 int main() {
-    std::cout << "Hello world! " << foo() << std::endl;
+
+    std::cout << "Hello world from sample: " << foo() << std::endl;
+
+    sf::Window App(sf::VideoMode(800, 600), "myproject");
+
+    while (App.isOpen()) {
+        sf::Event Event;
+        while (App.pollEvent(Event)) {
+            if (Event.type == sf::Event::Closed) {
+                App.close();
+            }
+        }
+        App.display();
+    }
 }
